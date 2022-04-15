098759a4ac6607f685e868075b8fc6d2ad350a2a
Hamzah Telha <50617242+Hamzah-Telha@users.noreply.github.com>
2022-04-14 17:05:50 -0700

    The first upload to github

---
+++ test_playlist_1/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace test_playlist_1
+{
+    partial class MusicPlayerApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MusicPlayerApp));
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbLogo = new System.Windows.Forms.Label();
+            this.listBoxSongs = new System.Windows.Forms.ListBox();
+            this.btnSelectSongs = new System.Windows.Forms.Button();
+            this.axWindowsMediaMusic = new AxWMPLib.AxWindowsMediaPlayer();
+            this.lblfooter = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.playlist_list = new System.Windows.Forms.ListBox();
+            this.TopPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaMusic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.BackColor = System.Drawing.SystemColors.Highlight;
+            this.TopPanel.Controls.Add(this.pictureBox1);
+            this.TopPanel.Controls.Add(this.lbLogo);
+            this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(1374, 47);
+            this.TopPanel.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1309, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(53, 47);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // lbLogo
+            // 
+            this.lbLogo.AutoSize = true;
+            this.lbLogo.Font = new System.Drawing.Font("Monotype Corsiva", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLogo.Location = new System.Drawing.Point(25, 13);
+            this.lbLogo.Name = "lbLogo";
+            this.lbLogo.Size = new System.Drawing.Size(136, 20);
+            this.lbLogo.TabIndex = 0;
+            this.lbLogo.Text = "Music Player App";
+            // 
+            // listBoxSongs
+            // 
+            this.listBoxSongs.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxSongs.FormattingEnabled = true;
+            this.listBoxSongs.ItemHeight = 19;
+            this.listBoxSongs.Location = new System.Drawing.Point(913, 91);
+            this.listBoxSongs.Name = "listBoxSongs";
+            this.listBoxSongs.Size = new System.Drawing.Size(242, 327);
+            this.listBoxSongs.TabIndex = 1;
+            this.listBoxSongs.SelectedIndexChanged += new System.EventHandler(this.listBoxSongs_SelectedIndexChanged);
+            // 
+            // btnSelectSongs
+            // 
+            this.btnSelectSongs.BackColor = System.Drawing.Color.Blue;
+            this.btnSelectSongs.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSelectSongs.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelectSongs.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSelectSongs.Location = new System.Drawing.Point(913, 424);
+            this.btnSelectSongs.Name = "btnSelectSongs";
+            this.btnSelectSongs.Size = new System.Drawing.Size(204, 61);
+            this.btnSelectSongs.TabIndex = 2;
+            this.btnSelectSongs.Text = "Selelct Video";
+            this.btnSelectSongs.UseVisualStyleBackColor = false;
+            this.btnSelectSongs.Click += new System.EventHandler(this.btnSelectSongs_Click);
+            // 
+            // axWindowsMediaMusic
+            // 
+            this.axWindowsMediaMusic.Enabled = true;
+            this.axWindowsMediaMusic.Location = new System.Drawing.Point(13, 71);
+            this.axWindowsMediaMusic.Name = "axWindowsMediaMusic";
+            this.axWindowsMediaMusic.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaMusic.OcxState")));
+            this.axWindowsMediaMusic.Size = new System.Drawing.Size(659, 401);
+            this.axWindowsMediaMusic.TabIndex = 3;
+            // 
+            // lblfooter
+            // 
+            this.lblfooter.AutoSize = true;
+            this.lblfooter.Font = new System.Drawing.Font("Microsoft Tai Le", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblfooter.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblfooter.Location = new System.Drawing.Point(471, 588);
+            this.lblfooter.Name = "lblfooter";
+            this.lblfooter.Size = new System.Drawing.Size(185, 18);
+            this.lblfooter.TabIndex = 4;
+            this.lblfooter.Text = "Developed By: Hamzah Telha";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.btnCreate.Location = new System.Drawing.Point(913, 491);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(204, 40);
+            this.btnCreate.TabIndex = 5;
+            this.btnCreate.Text = "Save Playlist";
+            this.btnCreate.UseVisualStyleBackColor = false;
+            this.btnCreate.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.btnLoad.Location = new System.Drawing.Point(913, 537);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(204, 36);
+            this.btnLoad.TabIndex = 6;
+            this.btnLoad.Text = "Load Playlist";
+            this.btnLoad.UseVisualStyleBackColor = false;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // playlist_list
+            // 
+            this.playlist_list.FormattingEnabled = true;
+            this.playlist_list.ItemHeight = 16;
+            this.playlist_list.Location = new System.Drawing.Point(1170, 91);
+            this.playlist_list.Name = "playlist_list";
+            this.playlist_list.Size = new System.Drawing.Size(192, 324);
+            this.playlist_list.TabIndex = 7;
+            this.playlist_list.SelectedIndexChanged += new System.EventHandler(this.playlist_list_SelectedIndexChanged);
+            // 
+            // MusicPlayerApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1374, 615);
+            this.Controls.Add(this.playlist_list);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.lblfooter);
+            this.Controls.Add(this.axWindowsMediaMusic);
+            this.Controls.Add(this.btnSelectSongs);
+            this.Controls.Add(this.listBoxSongs);
+            this.Controls.Add(this.TopPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MusicPlayerApp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Music Player App";
+            this.Load += new System.EventHandler(this.MusicPlayerApp_Load);
+            this.TopPanel.ResumeLayout(false);
+            this.TopPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaMusic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel TopPanel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbLogo;
+        private System.Windows.Forms.ListBox listBoxSongs;
+        private System.Windows.Forms.Button btnSelectSongs;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaMusic;
+        private System.Windows.Forms.Label lblfooter;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.ListBox playlist_list;
+    }
+}
+
---
+++ test_playlist_1/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+using Microsoft.Win32.SafeHandles;
+
+namespace test_playlist_1
+{
+    public partial class MusicPlayerApp : Form
+    {
+        public MusicPlayerApp()
+        {
+            InitializeComponent();
+        }
+
+        //Create Global Variables of String Type Array to save the titles or name of the Tracks and path of the track
+        String[] paths, files;
+
+        private void btnSelectSongs_Click(object sender, EventArgs e)
+        {
+            //Code to Select Songs
+            OpenFileDialog newPlaylist = new OpenFileDialog();
+            // the first place the program will select
+            newPlaylist.InitialDirectory = "C://your_path_that_will_select_the_target_file";
+            // it will determine the file type
+            newPlaylist.Filter = "MP3 Video File (*.mp4)|*.mp4|Windows Media File (*.avi)|*.avi|MKV Video File (*.mkv)|*.mkv|All Files (*.*)|*.*";
+            // restores the directory to the previously selected directory before closing
+            newPlaylist.RestoreDirectory = false;
+            // it allow muliple selection
+            newPlaylist.Multiselect = true;
+            // The dialog box return value is OK (usually sent from a button labeled OK)
+            if (newPlaylist.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // it will store the name of files
+                files = newPlaylist.SafeFileNames;
+                // it will store the path of files
+                paths = newPlaylist.FileNames;
+                for (int list = 0; list < files.Length; list++)
+                {
+                    // it will add the paths of media files to the list
+                    listBoxSongs.Items.Add(paths[list]);
+                }
+            }
+        }
+
+        // a function execute when the app on load
+        private void MusicPlayerApp_Load(object sender, EventArgs e)
+        {
+            // it manage the directories and subdirectories
+            DirectoryInfo dinfo =
+                new DirectoryInfo(
+                    "C://your_path_that_will_manage_directories_and_subdirectories");
+            FileInfo[] Files = dinfo.GetFiles("*.xml");
+            foreach (FileInfo file in Files)
+            {
+                // add the file name to the list of playlist
+                playlist_list.Items.Add(file.Name);
+            }
+        }
+
+        // the list box for video 
+        private void listBoxSongs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Write a code to play video
+            try
+            {
+                axWindowsMediaMusic.URL = Convert.ToString(listBoxSongs.SelectedItem);
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show("We can not play this Media");
+            }
+            
+        }
+
+        // The list box for play list
+        private void playlist_list_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            StreamReader playlist = new StreamReader(playlist_list.SelectedItem.ToString());
+            while (playlist.Peek() >= 0)
+                listBoxSongs.Items.Add(playlist.ReadLine());
+        }
+
+        // To close the program
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // To Save the play list xml file
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            StreamWriter Write;
+            SaveFileDialog savePlaylist = new SaveFileDialog();
+            // restores the directory to the previously selected directory before closing
+            savePlaylist.RestoreDirectory = false;
+            try
+            {
+                // the first place the program will select
+                savePlaylist.InitialDirectory = "C://your_path_that_will_save_the_target_file";
+                // it will determine the file type
+                savePlaylist.Filter = ("XML File|*.xml|All Files|*.*");
+                // show small window to choose file
+                savePlaylist.ShowDialog();
+
+                // To assign the xml file to the object Write
+                Write = new StreamWriter(savePlaylist.FileName);
+                
+                // To write all the list box items to the object Write
+                for (int I = 0; I < listBoxSongs.Items.Count; I++)
+                {
+                    // write the path of media file
+                    Write.Write(paths[I]);
+                    // write an empty line
+                    Write.WriteLine();
+                }
+                Write.Close();
+                MessageBox.Show("Playlist saved!");
+            }
+
+            catch 
+            {
+                return;
+            }
+        }
+
+        // To load a play list from xml file
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog loadPlaylist = new OpenFileDialog();
+            // it restricting muliple selection
+            loadPlaylist.Multiselect = false;
+
+            //// To eras all content of list box
+            this.listBoxSongs.Items.Clear();
+            
+            try
+            {
+                // show small dialog to choose file
+                loadPlaylist.ShowDialog();
+                // the first place the program will select
+
+                loadPlaylist.InitialDirectory = "C://your_path_that_will_load_the_target_file";
+                
+                 //To read in sequential the file from loadPlaylist to playlist to write on it
+                StreamReader playlist = new StreamReader(loadPlaylist.FileName);
+
+                while (playlist.Peek() >= 0)
+                {
+                    // write a single line to the playlist file
+                    listBoxSongs.Items.Add(playlist.ReadLine());
+                }
+
+                listBoxSongs.Text = loadPlaylist.FileName;
+            } catch {
+                return;
+            }
+        }
+    }
+}
